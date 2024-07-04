de2de72683199694adaa996638b7b2e97f0bc353
Ryan Chandler <ryangjchandler@gmail.com>
2024-07-04 16:16:18 +0100

    AST: Add a new Nodeable trait for AST nodes to implement

--- crates/pxp-ast/src/node.rs
+++ crates/pxp-ast/src/node.rs
@@ -3,11 +3,11 @@ use std::fmt::Debug;
 use pxp_span::{Span, Spanned};
 
 #[derive(Debug)]
-pub struct Node<'a, T: Debug + Spanned> {
+pub struct Node<'a, T: Debug + Spanned + Nodeable> {
     inner: &'a T,
 }
 
-impl<'a, T: Debug + Spanned> Node<'a, T> {
+impl<'a, T: Debug + Spanned + Nodeable> Node<'a, T> {
     pub fn new(inner: &'a T) -> Self {
         Self { inner }
     }
@@ -17,8 +17,12 @@ impl<'a, T: Debug + Spanned> Node<'a, T> {
     }
 }
 
-impl<'a, T: Debug + Spanned> Spanned for Node<'a, T> {
+impl<'a, T: Debug + Spanned + Nodeable> Spanned for Node<'a, T> {
     fn span(&self) -> Span {
         self.inner.span()
     }
+}
+
+pub trait Nodeable {
+    fn children(&self) -> Vec<&dyn Nodeable>;
 }
